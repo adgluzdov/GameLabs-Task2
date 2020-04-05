4a19c98d2bab42917ef5986c0314a8b8a2ee4c73
gluzdoff <adgluzdoff@gmail.com>
2020-04-05 20:22:13 +0300

    Написан алгоритм для поиска кратчайшего пути от произвольной станции в любую другую.

---
+++ Assets/Scripts/Metro/Metro.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metro
+{
+    private readonly List<Station> points;
+    private readonly Dictionary<Station, List<Tuple<Station, Line>>> edges;
+    public const int MAX = 2147483647;
+
+    public Metro(List<Station> points, Dictionary<Station, List<Tuple<Station, Line>>> edges)
+    {
+        this.points = points;
+        this.edges = edges;
+    }
+
+    public (List<Tuple<Station, Line>>, int) Find(Station start, Station finish)
+    {
+        Queue<Station> queue = new Queue<Station>();
+        Dictionary<Station, List<Tuple<Station, Line>>> paths = new Dictionary<Station, List<Tuple<Station, Line>>>();
+        int minTransfers = MAX;
+        Station current = start;
+
+        queue.Enqueue(current);
+        paths[current] = new List<Tuple<Station, Line>>();
+
+        while (queue.Count != 0)
+        {
+            current = queue.Dequeue();
+            if (paths.ContainsKey(finish))
+            {
+                if (paths[finish].Count == paths[current].Count)
+                {
+                    return (paths[finish], minTransfers);
+                }
+            }
+
+            foreach (var edge in edges[current])
+            {
+                var path = new List<Tuple<Station, Line>>(paths[current]); path.Add(edge);
+                if (edge.Item1 == finish) 
+                {
+                    if (!paths.ContainsKey(finish))
+                    {
+                        paths.Add(edge.Item1, path);
+                        minTransfers = CountTransfers(path);
+                        continue;
+                    }
+                    else 
+                    {
+                        if (minTransfers > CountTransfers(path))
+                        { 
+                            paths[edge.Item1] = path;
+                            minTransfers = CountTransfers(path);
+                            continue;
+                        }
+                    }
+                }
+                if (!paths.ContainsKey(edge.Item1))
+                {
+                    paths.Add(edge.Item1, path);
+                    queue.Enqueue(edge.Item1);
+                }
+            }
+        }
+
+        return (paths[finish], minTransfers);
+    }
+
+    public static int CountTransfers(List<Tuple<Station, Line>> path)
+    {
+        if (path.Count < 2)
+            return 0;
+
+        int number = 0;
+        for (int i = 1; i < path.Count; i++)
+        {
+            if (path[i - 1].Item2 != path[i].Item2)
+            {
+                number++;
+            }
+        }
+        return number;
+    }
+
+    public enum Line
+    {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        Black = 3,
+        White = 3,
+        Pink = 3,
+        Yellow = 3,
+    }
+
+    public enum Station
+    {
+        A = 0,
+        B = 1,
+        C = 2,
+        D = 3,
+        E = 4,
+        F = 5,
+        G = 6,
+        H = 7,
+        I = 8,
+        J = 9,
+        K = 10,
+        L = 11,
+        M = 12,
+        N = 13,
+        O = 14,
+        P = 15,
+        Q = 16,
+        R = 17,
+        S = 18,
+        T = 19,
+        U = 20,
+        V = 21,
+        W = 22,
+        X = 23,
+        Y = 24,
+        Z = 25,
+    }
+}
+
---
+++ Assets/Scripts/Tests/TestMetro.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using static Metro;
+
+namespace Tests
+{
+    public class TestMetro
+    {
+        List<Station> points = new List<Station>() { Station.A, Station.B, Station.C, Station.D, Station.E, Station.F, Station.G, Station.H, Station.J, Station.K, Station.L, Station.M, Station.N, Station.O };
+        Dictionary<Station, List<Tuple<Station, Line>>> edges = new Dictionary<Station, List<Tuple<Station, Line>>>() {
+            { Station.A,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.B, Line.Red)
+                }
+            },
+            { Station.B,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.A, Line.Red),
+                    Tuple.Create(Station.C, Line.Red),
+                    Tuple.Create(Station.H, Line.Black),
+                }
+            },
+            { Station.C,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.B, Line.Red),
+                    Tuple.Create(Station.D, Line.Red),
+                    Tuple.Create(Station.J, Line.Green),
+                    Tuple.Create(Station.K, Line.Green),
+                }
+            },
+            { Station.D,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.C, Line.Red),
+                    Tuple.Create(Station.E, Line.Red),
+                    Tuple.Create(Station.L, Line.Blue),
+                    Tuple.Create(Station.J, Line.Blue),
+                }
+            },
+            { Station.E,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.D, Line.Red),
+                    Tuple.Create(Station.F, Line.Red),
+                    Tuple.Create(Station.M, Line.Green),
+                    Tuple.Create(Station.J, Line.Green),
+                }
+            },
+            { Station.F,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.E, Line.Red),
+                    Tuple.Create(Station.J, Line.Black),
+                    Tuple.Create(Station.G, Line.Black),
+                }
+            },
+            { Station.G,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.F, Line.Black),
+                }
+            },
+            { Station.H,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.B, Line.Black),
+                    Tuple.Create(Station.J, Line.Black),
+                }
+            },
+            { Station.J,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.O, Line.Blue),
+                    Tuple.Create(Station.F, Line.Black),
+                    Tuple.Create(Station.E, Line.Green),
+                    Tuple.Create(Station.D, Line.Blue),
+                    Tuple.Create(Station.C, Line.Green),
+                    Tuple.Create(Station.H, Line.Black),
+                }
+            },
+            { Station.K,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.C, Line.Green),
+                    Tuple.Create(Station.L, Line.Green),
+                }
+            },
+            { Station.L,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.K, Line.Green),
+                    Tuple.Create(Station.M, Line.Green),
+                    Tuple.Create(Station.D, Line.Blue),
+                    Tuple.Create(Station.N, Line.Blue),
+                }
+            },
+            { Station.M,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.K, Line.Green),
+                    Tuple.Create(Station.M, Line.Green),
+                    Tuple.Create(Station.D, Line.Blue),
+                    Tuple.Create(Station.N, Line.Blue),
+                }
+            },
+            { Station.N,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.L, Line.Blue),
+                }
+            },
+            { Station.O,
+                new List<Tuple<Station, Line>>() {
+                    Tuple.Create(Station.J, Line.Blue),
+                }
+            },
+        };
+
+        [Test]
+        public void TestMetro1()
+        {
+            Metro metro = new Metro(points, edges);
+            (List<Tuple<Station, Line>> path, int transfers) = metro.Find(Station.A, Station.K);
+            Assert.AreEqual(path, new List<Tuple<Station, Line>>() {
+                Tuple.Create(Station.B, Line.Red),
+                Tuple.Create(Station.C, Line.Red),
+                Tuple.Create(Station.K, Line.Green),
+            });
+            Assert.AreEqual(transfers, 1);
+        }
+
+        [Test]
+        public void TestMetro2()
+        {
+            Metro metro = new Metro(points, edges);
+            (List<Tuple<Station, Line>> path, int transfers) = metro.Find(Station.B, Station.E);
+            Assert.AreEqual(path, new List<Tuple<Station, Line>>() {
+                Tuple.Create(Station.C, Line.Red),
+                Tuple.Create(Station.D, Line.Red),
+                Tuple.Create(Station.E, Line.Red),
+            });
+            Assert.AreEqual(transfers, 0);
+        }
+
+        [Test]
+        public void TestMetro3()
+        {
+            Metro metro = new Metro(points, edges);
+            (List<Tuple<Station, Line>> path, int transfers) = metro.Find(Station.L, Station.C);
+            Assert.AreEqual(path, new List<Tuple<Station, Line>>() {
+                Tuple.Create(Station.K, Line.Green),
+                Tuple.Create(Station.C, Line.Green),
+            });
+            Assert.AreEqual(transfers, 0);
+        }
+
+        [Test]
+        public void TestMetro4()
+        {
+            Metro metro = new Metro(points, edges);
+            (List<Tuple<Station, Line>> path, int transfers) = metro.Find(Station.C, Station.L);
+            Assert.AreEqual(path, new List<Tuple<Station, Line>>() {
+                Tuple.Create(Station.K, Line.Green),
+                Tuple.Create(Station.L, Line.Green),
+            });
+            Assert.AreEqual(transfers, 0);
+        }
+
+        [Test]
+        public void TestMetro5()
+        {
+            Metro metro = new Metro(points, edges);
+            (List<Tuple<Station, Line>> path, int transfers) = metro.Find(Station.G, Station.K);
+            Assert.AreEqual(path, new List<Tuple<Station, Line>>() {
+                Tuple.Create(Station.F, Line.Black),
+                Tuple.Create(Station.J, Line.Black),
+                Tuple.Create(Station.C, Line.Green),
+                Tuple.Create(Station.K, Line.Green),
+            });
+            Assert.AreEqual(transfers, 1);
+        }
+    }
+}
